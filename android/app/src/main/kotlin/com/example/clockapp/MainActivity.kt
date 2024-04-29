cae890b4ecb0c2c5c0c30884412692446958bda6
sajid <sajidshaikh12543@gmail.com>
2024-04-29 22:17:11 +0530

    29-04-2024

---
+++ android/app/src/main/kotlin/com/example/clockapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.clockapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/Lecture - 5.1/FlutterButtons.dart
@@ -0,0 +1,148 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Buttons(),
+    );
+  }
+}
+
+
+class Buttons extends StatefulWidget {
+  const Buttons({super.key});
+
+  @override
+  State<Buttons> createState() => _ButtonsState();
+}
+
+class _ButtonsState extends State<Buttons> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          'Actions',
+          style: TextStyle(
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          Container(
+            width: 360,
+            decoration: BoxDecoration(
+                border: Border.all(color: Colors.black),
+                borderRadius: BorderRadius.circular(10)
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  children: [
+                    ElevatedButton(onPressed: () {
+
+                    }, child: Text('Elevated')),
+                    FilledButton(onPressed: () {
+
+                    }, child: Text('Filled')),
+                    IconButton.filledTonal(onPressed: () {
+
+                    }, icon: Icon(Icons.add)),
+                    OutlinedButton(onPressed: () {
+
+                    }, child: Text('Outlined')),
+                    TextButton(onPressed: () {
+
+                    }, child: Text('Text'))
+                  ],
+                )
+              ],
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(top: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Floating Actions Buttons'),
+                Icon(Icons.info_outline),
+              ],
+            ),
+          ),
+          Container(
+            width: 360,
+            decoration: BoxDecoration(
+                border: Border.all(color: Colors.black),
+                borderRadius: BorderRadius.circular(10)
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton.filledTonal(onPressed: () {
+
+                }, icon: Icon(Icons.add)),
+                ElevatedButton(onPressed: () {
+
+                }, child: Icon(Icons.add)),
+                FilledButton(onPressed: () {
+
+                }, child: Icon(Icons.add)),
+                OutlinedButton(onPressed: () {
+
+                }, child:  Icon(Icons.add)),
+              ],
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(top: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Floating Actions Buttons'),
+                Icon(Icons.info_outline),
+              ],
+            ),
+          ),
+          Container(
+            width: 360,
+            decoration: BoxDecoration(
+                border: Border.all(color: Colors.black),
+                borderRadius: BorderRadius.circular(10)
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton.filledTonal(onPressed: () {
+
+                }, icon: Icon(Icons.settings_sharp)),
+                ElevatedButton(onPressed: () {
+
+                }, child: Icon(Icons.settings_sharp)),
+                FilledButton(onPressed: () {
+
+                }, child: Icon(Icons.settings_sharp)),
+                OutlinedButton(onPressed: () {
+
+                }, child: Icon(Icons.settings_sharp)),
+                TextButton(onPressed: () {
+
+                }, child: Icon(Icons.settings_sharp))
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
